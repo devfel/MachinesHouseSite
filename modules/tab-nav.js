0a0b519b2b1afaec934d2f0a393258266ef13033
Felizardo <devfelizardo@gmail.com>
2020-08-14 17:10:28 -0300

    tab-nav translation to english and refactoring to a class

--- modules/tab-nav.js
+++ modules/tab-nav.js
@@ -1,23 +1,40 @@
-// 0309 Criando Tabulação no Site, clica na imagem para mostrar o conteudo correto em texto.
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll(`[data-tab="menu"] li`);
-  const tabContent = document.querySelectorAll(`[data-tab="content"] section`);
+// Function that create the tab navigation on the website.
+// ie: clicking on a image to show/hide a content.
 
-  function activeTab(index) {
-    tabContent.forEach((elem) => {
-      elem.classList.remove(`ativo`);
-    });
-    const direcaoAnimacao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add(`ativo`, direcaoAnimacao);
+// --- Parameters ---
+// menu: used to select the elements to be clicked. ie: `[data="menu"] li`
+// default menu: none, can't be empty
+// content: used to select the elements to be shown: ie: `[data="content"] section`
+// default content: none, can't be empty
+
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = `active`;
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add(`ativo`);
+  activeTab(index) {
+    this.tabContent.forEach((elem) => {
+      elem.classList.remove(this.activeClass);
+    });
+    const direcaoAnimacao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcaoAnimacao);
+  }
 
-    tabMenu.forEach((element, indice) => {
-      element.addEventListener(`click`, () => {
-        activeTab(indice);
+  // function that add events to the tabs.
+  addTabNavEvent() {
+    this.tabMenu.forEach((element, indexAux) => {
+      element.addEventListener("click", () => {
+        this.activeTab(indexAux);
       });
     });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.activeTab(0); // activate first tab;
+      this.addTabNavEvent();
+    }
   }
 }
