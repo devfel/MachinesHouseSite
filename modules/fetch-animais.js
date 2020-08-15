bf0baf6932e62d283d8e99f0ceccaf9f33e75984
Felizardo <devfelizardo@gmail.com>
2020-08-15 03:25:53 -0300

    anima-numb translation to english and refactoring to a class

--- modules/fetch-animais.js
+++ modules/fetch-animais.js
@@ -1,11 +1,11 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumbers from "./anima-numbers.js";
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
     const div = document.createElement("div");
-    div.classList.add("numero-animal");
+    div.classList.add("number-animal");
     div.classList.add("json-api");
-    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-number>${animal.total}</span>`;
 
     return div;
   }
@@ -14,13 +14,18 @@ export default function initFetchAnimais() {
     try {
       const animaisRespose = await fetch(url);
       const animaisJSON = await animaisRespose.json();
-      const numerosGrid = document.querySelector(".numeros-grid");
+      const numbersGrid = document.querySelector(".numbers-grid");
 
       animaisJSON.forEach((element) => {
         const divAnimal = createAnimal(element);
-        numerosGrid.appendChild(divAnimal);
+        numbersGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();
+      const animaNumbers = new AnimaNumbers(
+        `[data-number]`,
+        `.numbers`,
+        `ativo`
+      );
+      animaNumbers.init();
     } catch (erro) {
       console.log(erro);
     }
