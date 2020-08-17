64e5fe89232d684d9a0382ac7e2c3cc92f241998
Felizardo <devfelizardo@gmail.com>
2020-08-17 03:08:53 -0300

    menu mobile refactoring

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import TabNav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/horario-func.js";
 import feathStores from "./modules/fetch-stores.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
@@ -38,8 +38,10 @@ animaScroll.init();
 const dropdownMenu = new DropdownMenu(`[data-dropdown]`);
 dropdownMenu.init();
 
+const menuMobile = new MenuMobile(`[data-menu="button"]`, `[data-menu="list"]`);
+menuMobile.init();
+
 feathStores(`./storesapi.json`, `.numbers-grid`);
 fetchBitcoin(`https://blockchain.info/ticker`, `.btc-preco`);
 
-initMenuMobile();
 initFuncionamento();
